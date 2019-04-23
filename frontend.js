2383cfb7fb14a9eb72e06acab8b7cad5910ad6ce
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2019-04-23 02:18:07 +0200

    Options menu mockup added for the extension, gitignore added, lint

--- frontend.js
+++ frontend.js
@@ -1,9 +1,11 @@
-document.body.innerHTML = '<div style="padding: 1px 16px; background: #555; color: #f1f1f1;" id="myHeader"> <input type="text" id="ENS_url" value=""> </div>' + document.body.innerHTML;
+document.body.innerHTML =
+  '<div style="padding: 1px 16px; background: #555; color: #f1f1f1;" id="myHeader"> <input type="text" id="ENS_url" value=""> </div>' +
+  document.body.innerHTML;
 
 //document.body.innerHTML = '<script> window.onscroll = function() {myFunction()}; var header = document.getElementById("myHeader"); var sticky = header.offsetTop; function myFunction() { if (window.pageYOffset > sticky) { header.classList.add("sticky"); } else { header.classList.remove("sticky"); } } </script>' + document.body.innerHTML;
 
 function setENSurl(message) {
-	document.getElementById("ENS_url").value = message.response;	
+  document.getElementById('ENS_url').value = message.response;
 }
 
 function handleError(error) {
@@ -11,29 +13,29 @@ function handleError(error) {
 }
 
 function sendmsg() {
-	var url = window.location.href;
-	var ipfs_location = url.lastIndexOf("ipfs");
-	var ipfsaddress = url.substring(ipfs_location+5,url.length-1); //TODO: remove constants
+  var url = window.location.href;
+  var ipfs_location = url.lastIndexOf('ipfs');
+  var ipfsaddress = url.substring(ipfs_location + 5, url.length - 1); //TODO: remove constants
   var sending = browser.runtime.sendMessage({
-      greeting: ipfsaddress
-    });
-sending.then(setENSurl, handleError);  
+    greeting: ipfsaddress
+  });
+  sending.then(setENSurl, handleError);
 }
 
-sendmsg();  
+sendmsg();
 
 window.onblur = setOriginalTheme;
 window.onfocus = setAlmonitTheme;
 
 function setAlmonitTheme() {
   var sending = browser.runtime.sendMessage({
-      greeting: "set_almonit_theme"
-    });
+    greeting: 'set_almonit_theme'
+  });
 }
 
 function setOriginalTheme() {
-	console.log("setting original theme");
+  console.log('setting original theme');
   var sending = browser.runtime.sendMessage({
-      greeting: "set_original_theme"
-    });
+    greeting: 'set_original_theme'
+  });
 }
