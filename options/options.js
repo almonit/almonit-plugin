cfb8fe4a3b2269d1ef5d4622943c37c44373bba5
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2019-05-04 23:11:26 +0200

    Options radio group names fixed, example of dynamic selection added

--- options/options.js
+++ options/options.js
@@ -1,33 +1,38 @@
 function saveOptions(e) {
-  e.preventDefault();
-  console.log('Changes are saved!');
-  // browser.storage.sync.set({
-  //   metrics: document.querySelector("#metrics").value
-  // });
+    e.preventDefault();
+    console.log('Changes are saved!');
+    // browser.storage.sync.set({
+    //   metrics: document.querySelector("#metrics").value
+    // });
 }
 
 function restoreOptions() {
-  function setCurrentChoice(result) {
-    document.querySelector('#metrics').value = result.metrics || false;
-  }
+    function setCurrentChoice(result) {
+        document.querySelector('#metrics').value = result.metrics || false;
+    }
 
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  }
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
 
-  var getting = browser.storage.sync.get('metrics');
-  getting.then(setCurrentChoice, onError);
+    var getting = browser.storage.sync.get('metrics');
+    getting.then(setCurrentChoice, onError);
 }
 
 function hotkeyListener(e) {
-  console.log(String.fromCharCode(e.which));
+    console.log(String.fromCharCode(e.which));
 }
+
+// Examples of applying selected radio buttons (ethereum radio group)
+document.forms['settingsForm'].ethereum[1].checked=true;
+// Examples of applying selected radio buttons (gateway radio group)
+document.forms['settingsForm'].gateway[1].checked=true;
 
 // document.addEventListener("DOMContentLoaded", restoreOptions);
 document.getElementById('settingsForm').addEventListener('submit', saveOptions);
 document
-  .getElementById('hotkeyBarInput')
-  .addEventListener('keyup', hotkeyListener);
+    .getElementById('hotkeyBarInput')
+    .addEventListener('keyup', hotkeyListener);
 document
-  .getElementById('hotkeySettingsInput')
-  .addEventListener('keyup', hotkeyListener);
+    .getElementById('hotkeySettingsInput')
+    .addEventListener('keyup', hotkeyListener);
