1efa0a0e28b73dceda1e681dac4115b5a46f5266
eyalron33 <eyalron33@gmail.com>
2020-09-05 23:47:15 +0200

    bump version, fix typo in welcome screen

--- pages/welcome_screen.js
+++ pages/welcome_screen.js
@@ -1,6 +1,6 @@
 document
 	.getElementById('searchEngine')
-	.addEventListener('click', function(){window.location.replace("https://almonit.eth");});
+	.addEventListener('click', function(){window.location.replace("http://almonit.eth");});
 document
 	.getElementById('introductionToDwebsitesArticle')
 	.addEventListener('click', function(){window.location.replace("http://blog.almonit.eth/2020-05-21/Introduction_to_Dwebsitse.html");});
