80268601266e4a1db50dd881cea7a44b05f8fa01
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2021-03-21 13:52:01 +0100

    Replace web3, with eth subpackages, version bump

--- js/web3-wrapper/src/index.js
+++ js/web3-wrapper/src/index.js
@@ -1,4 +1,4 @@
-var Web3 = require('web3');
+var Web3 = require('web3-eth');
 
 var web3 = '';
 var web3Testnet = '';
@@ -18,27 +18,27 @@ var web3Testnet = '';
 module.exports = {
 	// Mainnet functions
 	connect_web3: function(node) {
-		web3 = new Web3(node);
+		web3 = new Web3.Eth(node);
 	},
 
 	getContenthash: function(name) {
-		return web3.eth.ens.getContenthash(name);
+		return web3.ens.getContenthash(name);
 	},
 
 	getContent: function(name) {
-		return web3.eth.ens.getContent(name);
+		return web3.ens.getContent(name);
 	},
 
 	getSkynet: function(name) {
-		return web3.eth.ens.getText(name,"skynet");
+		return web3.ens.getText(name,"skynet");
     	},
 
     	// Testnet functions
     	connectWeb3Testnet: function(node) {
-		web3Testnet = new Web3(node);
+		web3Testnet = new Web3.Eth(node);
 	},
 
 	getContenthashTestnet: function(name) {
-		return web3Testnet.eth.ens.getContenthash(name);
+		return web3Testnet.ens.getContenthash(name);
 	}
 };
