ff5549c811d331de27e32b9bdc15eceabfddf4c9
emrecan-s <emreca.sanli@gmail.com>
2021-10-05 00:09:32 +0200

    gateways are updated - testnet is enabled as default

--- resources/gateways.js
+++ resources/gateways.js
@@ -1,82 +1,83 @@
 var gatewaysDataJSON = `{
-  "ethereumGateways": {
-    "mainnet.infura.io/v3/4ff76c15e5584ee4ad4d0c248ec86e17": {
-      "key": "mainnet.infura.io/v3/4ff76c15e5584ee4ad4d0c248ec86e17", 
-      "name": "Infura", 
-      "address": "https://mainnet.infura.io/v3/4ff76c15e5584ee4ad4d0c248ec86e17"
-    }
-  },
   
-  "ipfsGateways": {
-    "ipfs.io": {
-      "key": "ipfs.io",
-      "name": "Ipfs",
-      "address": "https://ipfs.io"
+    "ethereumGateways": {
+      "mainnet.infura.io/v3/4ff76c15e5584ee4ad4d0c248ec86e17": {
+        "key": "mainnet.infura.io/v3/4ff76c15e5584ee4ad4d0c248ec86e17", 
+        "name": "Infura", 
+        "address": "https://mainnet.infura.io/v3/4ff76c15e5584ee4ad4d0c248ec86e17"
+      }
     },
-    "ipfs.infura.io": { 
-      "key": "ipfs.infura.io",
-      "name": "Infura",
-      "address": "https://ipfs.infura.io"
-    } ,
-    "cloudflare-ipfs.com": { 
-      "key": "cloudflare-ipfs.com",
-      "name": "Cloudflare",
-      "address": "https://cloudflare-ipfs.com"
+    
+    "ipfsGateways": {
+      "ipfs.io": {
+        "key": "ipfs.io",
+        "name": "Ipfs",
+        "address": "https://ipfs.io"
+      },
+      "ipfs.infura.io": { 
+        "key": "ipfs.infura.io",
+        "name": "Infura",
+        "address": "https://ipfs.infura.io"
+      } ,
+      "cloudflare-ipfs.com": { 
+        "key": "cloudflare-ipfs.com",
+        "name": "Cloudflare",
+        "address": "https://cloudflare-ipfs.com"
+      },
+      "gateway.pinata.cloud": { 
+        "key": "gateway.pinata.cloud",
+        "name": "Pinata",
+        "address": "https://gateway.pinata.cloud"
+      },
+      "ipfs.sloppyta.co": {
+        "key": "ipfs.sloppyta.co",
+        "name": "Sloppyta.co",
+        "address": "https://ipfs.sloppyta.co"
+      },
+      "dweb.link": {
+        "key": "dweb.link",
+        "name": "Dweb.link",
+        "address": "https://dweb.link"
+      }
     },
-    "gateway.pinata.cloud": { 
-      "key": "gateway.pinata.cloud",
-      "name": "Pinata",
-      "address": "https://gateway.pinata.cloud"
+  
+    "skynetGateways": {
+      "siasky.net": {
+        "key": "siasky.net", 
+        "name": "Siasky", 
+        "address": "https://siasky.net"
+      }, 
+      "skydrain.net": { 
+        "key": "skydrain.net",
+        "name": "Skydrain",
+        "address": "https://skydrain.net"
+      },
+      "sialoop.net": { 
+        "key": "sialoop.net",
+        "name": "Sialoop",
+        "address": "https://sialoop.net"
+      },
+      "siacdn.com": { 
+        "key": "siacdn.com",
+        "name": "Siacdn",
+        "address": "https://www.siacdn.com"
+      },
+      "skynethub": { 
+        "key": "skynethub",
+        "name": "Skynethub",
+        "address": "https://skynethub.io"
+      },
+      "skynet.tutemwesi.com": { 
+        "key": "skynet.tutemwesi.com",
+        "name": "Tutemwesi",
+        "address": "https://skynet.tutemwesi.com"
+      },
+      "vault.lightspeedhosting.com": { 
+        "key": "vault.lightspeedhosting.com",
+        "name": "Lightspeed Hosting",
+        "address": "https://vault.lightspeedhosting.com"
+      }
     },
-    "ipfs.sloppyta.co": {
-      "key": "ipfs.sloppyta.co",
-      "name": "Sloppyta.co",
-      "address": "https://ipfs.sloppyta.co"
-    },
-    "dweb.link": {
-      "key": "dweb.link",
-      "name": "Dweb.link",
-      "address": "https://dweb.link"
-    }
-  },
-
-  "skynetGateways": {
-    "siasky.net": {
-      "key": "siasky.net", 
-      "name": "Siasky", 
-      "address": "https://siasky.net"
-    }, 
-    "skydrain.net": { 
-      "key": "skydrain.net",
-      "name": "Skydrain",
-      "address": "https://skydrain.net"
-    },
-    "sialoop.net": { 
-      "key": "sialoop.net",
-      "name": "Sialoop",
-      "address": "https://sialoop.net"
-    },
-    "siacdn.com": { 
-      "key": "siacdn.com",
-      "name": "Siacdn",
-      "address": "https://www.siacdn.com"
-    },
-    "skynethub": { 
-      "key": "skynethub",
-      "name": "Skynethub",
-      "address": "https://skynethub.io"
-    },
-    "skynet.tutemwesi.com": { 
-      "key": "skynet.tutemwesi.com",
-      "name": "Tutemwesi",
-      "address": "https://skynet.tutemwesi.com"
-    },
-    "vault.lightspeedhosting.com": { 
-      "key": "vault.lightspeedhosting.com",
-      "name": "Lightspeed Hosting",
-      "address": "https://vault.lightspeedhosting.com"
-    }
-  },
 
   "ethereumTestnets": {
     "rinkeby": {
